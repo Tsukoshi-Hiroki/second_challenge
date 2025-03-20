afa78c0a389118bd85850d342f8c83f31b441da9
Tsukoshi-Hiroki <ee213038@meiji.ac.jp>
2025-03-20 14:20:32 +0900

    temporarily-build -finish

--- include/second_challenge/second_challenge.hpp
+++ include/second_challenge/second_challenge.hpp
@@ -2,12 +2,39 @@
 #define SECOND_CHALLENGE_HPP
 
 #include <chrono>
+#include "roomba_500driver_meiji/msg/roomba_ctrl.hpp"
+#include <functional>
+#include <memory>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <optional>
+#include <rclcpp/rclcpp.hpp>
 
 using namespace std::chrono_literals;
 
 class SecondChallenge : public rclcpp::Node
 {
+public:
+  SecondChallenge();
+
+private:
+  // 関数
+  void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+  void timer_callback();
+  void is_goal(std::optional<sensor_msgs::msg::LaserScan> scan_);
+  double calc_min_dist();
+  void run(double velocity, double omega);
+
+  // pub sub
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
+  rclcpp::Publisher<roomba_500driver_meiji::msg::RoombaCtrl>::SharedPtr cmd_vel_pub_;
   rclcpp::TimerBase::SharedPtr timer_;
+
+  // 変数
+  std::optional<sensor_msgs::msg::LaserScan> scan_;
+  roomba_500driver_meiji::msg::RoombaCtrl cmd_vel_;
+  double goal_dist = 1.0;
+  double velocity = 0.0;
+  double omega = 0.0;
 };
 
 #endif  // SECOND_CHALLENGE_HPP
--- src/second_challenge.cpp
+++ src/second_challenge.cpp
@@ -1,14 +1,65 @@
 #include "second_challenge/second_challenge.hpp"
 
-SecondChallenge::SecondChallenge()
-: Node("second_challenge")
+SecondChallenge::SecondChallenge() : Node("second_challenge")
 {
   // timer
-  timer_ = this->create_wall_timer(<周期間隔>, std::bind(&<class名>::<callback関数名, this));
+  timer_ = this->create_wall_timer(100ms, std::bind(&SecondChallenge::timer_callback, this));
+
+  scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+      "/scan", rclcpp::QoS(1).reliable(),
+      std::bind(&SecondChallenge::scan_callback, this, std::placeholders::_1));
+
+  cmd_vel_pub_ =
+      this->create_publisher<roomba_500driver_meiji::msg::RoombaCtrl>(
+          "/roomba/control", rclcpp::QoS(1).reliable());
 }
 
+
+  // 一定周期で行う処理を書く
 void SecondChallenge::timer_callback()
 {
-    // 一定周期で行う処理を書く
+  if(scan_)
+  {
+    if(calc_min_dist() > 1.0)
+    {
+      run(0.1,0.0);
+    }else{
+      run(0.0,0.0);
+    }
+
+  }else
+  {
+    run(0.0,0.0);
+  }
 }
 
+// Lidarのコールバック
+void SecondChallenge::scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+{
+  scan_ = *msg;
+}
+
+//センサの最小値を算出
+double SecondChallenge::calc_min_dist()
+{
+  double all_angle = scan_.value().angle_max - scan_.value().angle_min;
+  double min_range = scan_.value().range_min;
+
+  RCLCPP_INFO(this->get_logger(), "angle_min = %lf",scan_.value().angle_min);
+  RCLCPP_INFO(this->get_logger(), "angle_max = %lf",scan_.value().angle_max);
+  RCLCPP_INFO(this->get_logger(), "all_angle = %lf",all_angle);
+  RCLCPP_INFO(this->get_logger(), "angle_increment = %lf",scan_.value().angle_increment);
+  RCLCPP_INFO(this->get_logger(), "range_min = %lf",scan_.value().range_min);
+
+  return min_range;
+}
+
+void SecondChallenge::run(double velocity, double omega) {
+  RCLCPP_INFO(this->get_logger(), "run");
+
+  cmd_vel_.mode = 11;
+  cmd_vel_.cntl.linear.x = velocity;
+  cmd_vel_.cntl.angular.z = omega;
+
+  cmd_vel_pub_->publish(cmd_vel_);
+}
